fafa33f0b413eeba02f47d0f638b7acf182e1d3d
humbug83 <m.fresi83@gmail.com>
2020-03-04 11:48:39 +0100

    Update structure.sh

--- structure.sh
+++ structure.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+USB_DRIVE=$(mount | grep media | awk '{print $3}')
+
 echo "Creating folder structure ..."
-mkdir -p $(mount | grep media | awk '{print $3}')/retro_pi/retro_home
-mkdir -p $(mount | grep media | awk '{print $3}')/retro_pi/emulationstation
-mkdir -p $(mount | grep media | awk '{print $3}')/retro_pi/retroarch
+mkdir -p ${USB_DRIVE}/retro_pi/retro_home
+mkdir -p ${USB_DRIVE}/retro_pi/emulationstation
+mkdir -p ${USB_DRIVE}/retro_pi/retroarch
 echo "...done!"
+
 echo "Start copying folders and files..."
 echo " ... copy folder retropie ..."
-sudo cp -R /home/pi/RetroPie/* $(mount | grep media | awk '{print $3}')/retro_pi/retro_home
+sudo cp -R /home/pi/RetroPie/* ${USB_DRIVE}/retro_pi/retro_home
+
 echo " ... copy folder themes ..."
-sudo cp -R /etc/emulationstation/themes $(mount | grep media | awk '{print $3}')/retro_pi/
+sudo cp -R /etc/emulationstation/themes ${USB_DRIVE}/retro_pi/
+
 echo " ... copy folder gamelists ..."
-sudo cp -R /opt/retropie/configs/all/emulationstation/gamelists $(mount | grep media | awk '{print $3}')/retro_pi/emulationstation/
+sudo cp -R /opt/retropie/configs/all/emulationstation/gamelists ${USB_DRIVE}/retro_pi/emulationstation/
+
 echo " ... copy folder autoconfig ..."
-sudo cp -R /opt/retropie/configs/all/retroarch/autoconfig $(mount | grep media | awk '{print $3}')/retro_pi/retroarch/
+sudo cp -R /opt/retropie/configs/all/retroarch/autoconfig ${USB_DRIVE}/retro_pi/retroarch/
+
 echo " ... copy folder config ..."
-sudo cp -R /opt/retropie/configs/all/retroarch/config $(mount | grep media | awk '{print $3}')/retro_pi/retroarch/
+sudo cp -R /opt/retropie/configs/all/retroarch/config ${USB_DRIVE}/retro_pi/retroarch/
+
 echo " ... copy config files ..."
-sudo cp /opt/retropie/configs/all/emulationstation/es_input.cfg $(mount | grep media | awk '{print $3}')/retro_pi/emulationstation/
-sudo cp /opt/retropie/configs/all/emulationstation/es_settings.cfg $(mount | grep media | awk '{print $3}')/retro_pi/emulationstation/
-sudo cp /opt/retropie/configs/all/emulationstation/es_systems.cfg $(mount | grep media | awk '{print $3}')/retro_pi/emulationstation/
+sudo cp /opt/retropie/configs/all/emulationstation/es_input.cfg ${USB_DRIVE}/retro_pi/emulationstation/
+sudo cp /opt/retropie/configs/all/emulationstation/es_settings.cfg ${USB_DRIVE}/retro_pi/emulationstation/
+sudo cp /opt/retropie/configs/all/emulationstation/es_systems.cfg ${USB_DRIVE}/retro_pi/emulationstation/
 echo "Done!"
